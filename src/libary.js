3a944889ec3aba20d5fdc049aea19acf4d2b57c0
AlekasndrHavryliuk <gavrfavorite@gmail.com>
2023-01-30 17:07:34 +0200

    Test

--- src/libary.js
+++ src/libary.js
@@ -1,66 +1,66 @@
-import { theMovieAPI } from './scripts/movieAPI';
-import { modalPopup } from './scripts/modalPopup';
-import { render } from './scripts/renderMarkup';
-import { refs } from './scripts/refs';
-import { savedFilms } from './scripts/serialize';
-import { removeMovieFromLibary } from './scripts/libaryManager';
+// import { theMovieAPI } from './scripts/movieAPI';
+// import { modalPopup } from './scripts/modalPopup';
+// import { render } from './scripts/renderMarkup';
+// import { refs } from './scripts/refs';
+// import { savedFilms } from './scripts/serialize';
+// import { removeMovieFromLibary } from './scripts/libaryManager';
 
-const movieAPI = new theMovieAPI();
+// const movieAPI = new theMovieAPI();
 
-const start = () => {
-  if (document.location.href.includes(`libary`)) {
-    refs.libaryWatchedBtn.addEventListener(`click`, savedFilms.filterMovieList);
-    refs.libaryQueueBtn.addEventListener(`click`, savedFilms.filterMovieList);
-    refs.libaryAllBtn.addEventListener(`click`, savedFilms.filterMovieList);
-    // refs.libaryGalleryEl.addEventListener(`dblclick`, removeMovie);
-    refs.libaryGalleryEl.addEventListener(`click`, openTrailerPopup);
-    loadLibaryFilms();
-  }
-};
+// const start = () => {
+//   if (document.location.href.includes(`libary`)) {
+//     refs.libaryWatchedBtn.addEventListener(`click`, savedFilms.filterMovieList);
+//     refs.libaryQueueBtn.addEventListener(`click`, savedFilms.filterMovieList);
+//     refs.libaryAllBtn.addEventListener(`click`, savedFilms.filterMovieList);
+//     // refs.libaryGalleryEl.addEventListener(`dblclick`, removeMovie);
+//     refs.libaryGalleryEl.addEventListener(`click`, openTrailerPopup);
+//     loadLibaryFilms();
+//   }
+// };
 
-const removeMovie = id => removeMovieFromLibary(id);
+// const removeMovie = id => removeMovieFromLibary(id);
 
-const loadTrailer = async id => {
-  const { results } = await movieAPI.fetchTreiler(id);
-  console.log(results);
-  let offTrailerKey = null;
-  results.map(element => {
-    if (element.name.toLowerCase().includes(`official trailer`)) {
-      offTrailerKey = element.key;
-    }
-  });
-  if (offTrailerKey === null) offTrailerKey = results[0].key;
-  render.trailerPopup(offTrailerKey);
-};
+// const loadTrailer = async id => {
+//   const { results } = await movieAPI.fetchTreiler(id);
+//   console.log(results);
+//   let offTrailerKey = null;
+//   results.map(element => {
+//     if (element.name.toLowerCase().includes(`official trailer`)) {
+//       offTrailerKey = element.key;
+//     }
+//   });
+//   if (offTrailerKey === null) offTrailerKey = results[0].key;
+//   render.trailerPopup(offTrailerKey);
+// };
 
-const openTrailerPopup = event => {
-  const { target, ctrlKey } = event;
-  const id = target.dataset.movieId;
-  console.log(id != undefined && ctrlKey);
-  if (id && ctrlKey) {
-    removeMovie(id);
-    return;
-  }
-  if (id != undefined) loadTrailer(id);
-  modalPopup.toggleHide();
-  modalPopup.addAnimattion();
-  modalPopup.addModalListeners();
-};
+// const openTrailerPopup = event => {
+//   const { target, ctrlKey } = event;
+//   const id = target.dataset.movieId;
+//   console.log(id != undefined && ctrlKey);
+//   if (id && ctrlKey) {
+//     removeMovie(id);
+//     return;
+//   }
+//   if (id != undefined) loadTrailer(id);
+//   modalPopup.toggleHide();
+//   modalPopup.addAnimattion();
+//   modalPopup.addModalListeners();
+// };
 
-const loadLibaryFilms = async () => {
-  refs.libaryGalleryEl.innerHTML = '';
-  const films = savedFilms.getAllFilms();
-  try {
-    if (films.length === 0) return;
-    for (let i = 0; i < films.length; i++) {
-      const result = await movieAPI.fetchMovieById(films[i]);
-      render.libaryCard(result);
-    }
-  } catch (err) {
-    console.log(err);
-  }
-};
+// const loadLibaryFilms = async () => {
+//   refs.libaryGalleryEl.innerHTML = '';
+//   const films = savedFilms.getAllFilms();
+//   try {
+//     if (films.length === 0) return;
+//     for (let i = 0; i < films.length; i++) {
+//       const result = await movieAPI.fetchMovieById(films[i]);
+//       render.libaryCard(result);
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-// window.addEventListener(`load`, start)
+// // window.addEventListener(`load`, start)
 
-console.log(`Ok`)
+// console.log(`Ok`)
--- src/scripts/libaryManager.js
+++ src/scripts/libaryManager.js
@@ -1,24 +1,24 @@
-import { refs } from './refs';
-import { savedFilms } from './serialize';
+// import { refs } from './refs';
+// import { savedFilms } from './serialize';
 
-export const removeMovieFromLibary = id => {
-  localStorage.clear();
-  savedFilms.queueFilms = savedFilms.queueFilms.filter(
-    element => element != id
-  );
-  savedFilms.watchedFilms = savedFilms.watchedFilms.filter(
-    element => element != id
-  );
-  const galleryCardsLibEl = document.querySelectorAll(`.gallery__card-libary`);
-  console.log(galleryCardsLibEl);
-  galleryCardsLibEl.forEach(element => {
-    console.log(element.dataset.movieId);
-    if (element.dataset.movieId === id) {
-      element.remove();
-    }
-  });
-  if (savedFilms.watchedFilms.length != 0);
-  localStorage.setItem(`watched-movie-list`, savedFilms.watchedFilms);
-  if (savedFilms.queueFilms.length != 0)
-    localStorage.setItem(`queue-movie-list`, savedFilms.queueFilms);
-};
+// export const removeMovieFromLibary = id => {
+//   localStorage.clear();
+//   savedFilms.queueFilms = savedFilms.queueFilms.filter(
+//     element => element != id
+//   );
+//   savedFilms.watchedFilms = savedFilms.watchedFilms.filter(
+//     element => element != id
+//   );
+//   const galleryCardsLibEl = document.querySelectorAll(`.gallery__card-libary`);
+//   console.log(galleryCardsLibEl);
+//   galleryCardsLibEl.forEach(element => {
+//     console.log(element.dataset.movieId);
+//     if (element.dataset.movieId === id) {
+//       element.remove();
+//     }
+//   });
+//   if (savedFilms.watchedFilms.length != 0);
+//   localStorage.setItem(`watched-movie-list`, savedFilms.watchedFilms);
+//   if (savedFilms.queueFilms.length != 0)
+//     localStorage.setItem(`queue-movie-list`, savedFilms.queueFilms);
+// };
